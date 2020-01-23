7304ec9dc976ab4bd8c0886705a36116352b8dfd
Paloma D <pvc_dl@hotmail.com>
2020-01-23 14:51:36 -0300

    Avance

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+/* function iconPressed() {
+    //función que se ejecuta al presionar el botón pre seleccionada
+    document.getElementById("categories").style.display = "none";
+    document.getElementById("categoryPreSeleccionada").style.display = "block"; */
